bda2ba4afdcb732e40b7ce6d240ed13113c1d924
MohcineDev <mouhcinedev@gmail.com>
2021-01-02 11:11:40 +0000

    input eventListener - year func

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const scrollTop = document.querySelector('.scroll')
+const year = document.querySelector('.copyright span')
+const email = document.querySelector('#email')
 
 const height = innerHeight
 
 document.addEventListener('scroll', () => {
 
-    if (scrollY >= height * 2) 
+    if (scrollY >= height * 2)
         scrollTop.style.display = 'block'
     else
         scrollTop.style.display = 'none'
@@ -18,3 +20,14 @@ scrollTop.addEventListener('click', () => {
     })
 
 })
+
+year.textContent = new Date().getFullYear()
+
+
+//placeholder name@mail.com
+let qualifiedName = email.getAttribute('placeholder')
+
+email.addEventListener('focus', () => email.setAttribute('placeholder', ''))
+email.addEventListener('blur', () => email.setAttribute('placeholder', qualifiedName))
+
+
